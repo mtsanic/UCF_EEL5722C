79e0aa83ee6c4ba328248c91fc1bad56d868b106
mtsanic <48995593+mtsanic@users.noreply.github.com>
2023-12-08 18:53:47 -0500

    Add files via upload

---
+++ Lab5/sources/clk_gen.v
@@ -0,0 +1,140 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/21/2023 11:53:07 AM
+// Design Name: 
+// Module Name: clk_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Clock Generation for 10Hz and 500Hz
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//Clock Divider for 10Hz
+//Used for Decoder control
+module clk_gen_20Hz(
+    clk,        //|<i
+    rst,        //|<i
+    clk_20Hz    //|>o
+    );
+
+input clk;
+input rst;
+output reg clk_20Hz;
+
+reg [22:0] counter;
+
+initial begin
+    clk_20Hz<=0;
+    counter<=0;
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        counter<=0;
+        clk_20Hz<=0;
+    end
+    else begin
+        if(counter==4_999_999) begin    //100M/(5M)=20Hz
+            clk_20Hz<=~clk_20Hz;
+            counter<=0;
+        end
+        else begin
+            counter<=counter+1;
+        end
+    end
+end
+
+
+endmodule
+
+//Clock Divider for 1kHz
+//Used for 7segment display
+module clk_gen_1kHz(
+    clk,        //|<i
+    rst,        //|<i
+    clk_1kHz    //|>o
+    );
+
+input clk;
+input rst;
+output reg clk_1kHz;
+
+reg [16:0] counter;
+
+initial begin
+    clk_1kHz<=0;
+    counter<=0;
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        counter<=0;
+        clk_1kHz<=0;
+    end
+    else begin
+        if(counter==99_999) begin   //100M/(100k)=1kHz
+            clk_1kHz<=~clk_1kHz;
+            counter<=0;
+        end
+        else begin
+            counter<=counter+1;
+        end
+    end
+end
+
+endmodule
+
+
+//Clock Divider for 25MHz
+//Used for VGA display
+module clk_gen_50MHz(
+    clk,        //|<i
+    rst,        //|<i
+    clk_50MHz   //|>o
+    );
+
+input clk;
+input rst;
+output reg clk_50MHz;
+
+reg [1:0] counter;
+
+initial begin
+    clk_50MHz<=0;
+    counter<=0;
+end
+
+always @(posedge clk ) begin
+    if (rst) begin
+        clk_50MHz<=0;
+        counter<=0;
+    end
+    else begin
+       if(counter==1) begin   //100M/(2*2)=25MHz
+            clk_50MHz=~clk_50MHz;
+            counter=0;
+        end
+        else begin
+            counter=counter+1;
+        end 
+    end   
+end
+
+endmodule
---
+++ Lab5/sources/vga_cntl.v
@@ -0,0 +1,118 @@
+`timescale 1ns / 1ps
+
+
+module vga_cntl(
+    clk_50MHz,  //|<i
+    rst,        //|<i
+    Hsync,      //|>o
+    Vsync,      //|>o
+    x_pos,      //|>o
+    y_pos       //|>o
+    );
+
+
+    localparam h_pixel   = 800;
+    localparam h_display = 640;
+    localparam h_pulse   = 96;
+    localparam h_fp      = 16;
+    localparam h_bp      = 48;
+
+    localparam v_pixel   = 521;
+    localparam v_display = 480;
+    localparam v_pulse   = 2;
+    localparam v_fp      = 10;
+    localparam v_bp      = 29;
+
+    input clk_50MHz;
+    input rst;
+    output Hsync;
+    output Vsync;
+    output [9:0] x_pos;
+    output [9:0] y_pos;
+
+    reg [9:0] row_pix=0;
+    reg [9:0] col_pix=0;
+
+
+    always @(posedge clk_50MHz or posedge rst) begin
+        if (rst) begin
+            col_pix=0;
+            row_pix=0;
+        end
+        else begin
+            if(row_pix==(h_pixel-1)) begin
+                if(col_pix==(v_pixel-1)) begin
+                    col_pix=0;
+                    row_pix=0;
+                end
+                else begin
+                    col_pix=col_pix+1;
+                    row_pix=0;
+                end
+            end
+            else begin
+                row_pix=row_pix+1;
+            end
+        end
+    end
+
+    
+//    always @(posedge clk_50MHz or posedge rst) begin
+//        if (rst) begin
+//            col_pix=0;
+//            row_pix=0;
+//        end
+//        else begin
+//            if(row_pix==(h_pixel-1) && col_pix==(v_pixel-1)) begin
+//                    col_pix=0;
+//                    row_pix=0;
+//            end
+//            else if(row_pix==(h_pixel-1) && col_pix!=(v_pixel-1)) begin
+//                    col_pix=col_pix+1;
+//                    row_pix=0;
+//            end
+//            else begin
+//                row_pix=row_pix+1;
+//            end
+//        end
+//    end
+
+
+//    always @(posedge clk_50MHz or posedge rst) begin
+//        if (rst) begin
+//            row_pix<=0;
+//        end
+//        else begin
+//            if(row_pix==(h_pixel-1)) begin
+//                row_pix=0;
+//            end
+//            else begin
+//                row_pix=row_pix+1;
+//            end
+//        end
+//    end
+
+//    always @(posedge clk_50MHz or posedge rst) begin
+//        if (rst) begin
+//            col_pix<=0;
+//        end
+//        else begin
+//            if(row_pix==(h_pixel-1)) begin
+//                if (row_pix==(v_pixel-1)) begin
+//                   col_pix=0; 
+//                end  
+//            end
+//            else begin
+//                col_pix=col_pix+1;
+//            end
+//        end
+//    end
+
+    assign Hsync=(row_pix< h_pulse)? 0:1;
+    assign Vsync =(col_pix < v_pulse)? 0:1;
+
+    assign x_pos=row_pix;
+    assign y_pos=col_pix;
+
+
+endmodule
